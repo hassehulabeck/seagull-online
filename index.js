5854172c752f3f0304a58de8b7b2bf6872b614ea
hassehulabeck <hansbudandersson@gmail.com>
2020-03-06 11:44:34 +0100

    Shows members by activity + filter by activity

--- index.js
+++ index.js
@@ -2,19 +2,29 @@ const express = require('express')
 const app = express()
 const pool = require('./pool.js')
 
-// Denna query använder en placeholder (frågetecknet)
-let query = "SELECT * FROM members WHERE id=?"
-let userID
-
 app.get('/', (req, res) => {
-    userID = req.query.id
+    res.send("/members för medlemmar som har någon aktivitet, /members?activity=2 för alla medlemmar som håller på med skidor.")
+})
+
+app.get('/members', (req, res) => {
+    // Ternary operator på actString utifrån query i URLen.
+    let actString = req.query.activity ? " activity = ?" : " ?"
+    let activity = req.query.activity ? [req.query.activity] : [1]
+
+    let query = `SELECT firstName, lastName, teams.name, activities.name 
+    FROM members
+    JOIN members_teams ON members_teams.memberID = members.id
+    JOIN teams ON members_teams.teamID = teams.id
+    JOIN activities ON activities.id = teams.activity
+    WHERE` + actString
+
     pool((err, connection) => {
         // Arrayen som innehåller userID innehåller de värden som kommer att ersätta frågetecknet.
-        connection.query(query, [userID], (error, result, fields) => {
+        connection.query(query, [activity], (error, result, fields) => {
             connection.release()
 
             // Kolla om error, behandla resultatet.
-            if (error) throw error
+            if (err) throw err
 
             res.json(result)
 
